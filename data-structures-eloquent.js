22e9de248449a3a6578cbb3adab369f657c9a233
travis <trvsmartin@gmail.com>
2023-01-03 14:36:45 -0800

    chapter 4 exercises
    <body>
    
    Issue #

--- data-structures-eloquent.js
+++ data-structures-eloquent.js
@@ -12,4 +12,107 @@ Object.assign: copies all properties from one object into another
 
 <for-of> loop: works for arrays, strings, some others
 
+Rest Parameters: for a function to accept any number of arguments.  rest parameter bound to an array with all further args.
+-calling a function with an array of args spreads the array, passing element as separate arguments
+-square bracket notation allows spread operator to spread an array into another eg: words = ['never', 'fully']; phrase = ['will', ...words, 'understand']
+
+A List is a common data structure: nested set of objects, the first holding a reference to the seond, the second to the third, etc
+-benefit of lists: shared parts of structure
 */
+
+// rest parameter example
+let max = (...numbers) => {
+  let result = -Infinity;
+  for (let number of numbers) {
+    if (number > result) result = number;
+  }
+  return result;
+};
+
+// destructuring:
+function phi(table) {
+  return (
+    (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt(
+      (table[2] + table[3]) *
+        (table[0] + table[1]) *
+        (table[1] + table[3]) *
+        (table[0] + table[2])
+    )
+  );
+}
+// is equivalent to
+function phi([n00, n01, n10, n11]) {
+  return (
+    (n11 * n00 - n10 * n01) /
+    Math.sqrt((n10 + n11) * (n00 + n01) * (n01 + n11) * (n00 + n10))
+  );
+}
+// square bracket destructuring lets us 'name' array elements
+
+// similarly you can destructure from an object using braces:
+let { age } = { name: "Faraji", age: 23 };
+console.log(age);
+
+/* Chapter 4 Exercises */
+// Range: write a range function that takes two arguements (start and end), and returns an array containing all the numbers from start to end inclusive
+const range = (start, end) => {
+  let startToEnd = [];
+  for (let index = start; index <= end; index++) {
+    startToEnd.push(index);
+  }
+  console.log(startToEnd);
+};
+
+// Sum: Write a sum function that takes an array of numbers and returns the sum of these
+const sum = (numbers) => {
+  let result = 0;
+  for (let number of numbers) {
+    result += number;
+  }
+  return console.log(result);
+};
+
+// Reversing an Array: write a function to reverse as the array.reverse method, take an array as argument, produces new array with same elements
+const reverseArray = (array) => {
+  let reversed = [];
+  for (let item of array) {
+    reversed.unshift(item);
+  }
+  return console.log(reversed);
+};
+
+// write a function reverseArrayInPlace: modify the array given by reversing its elements, return the original
+const reverseArrayInPlace = (array) => {
+  let halfway = Math.floor(array.length / 2);
+  let last = array.length - 1;
+  for (let i = 0; i < halfway; i++) {
+    let current = array[i];
+    array[i] = array[last - i];
+    array[last - i] = current;
+  }
+  return console.log(array);
+};
+
+// A List: write a function arrayToList that builds up a list structure
+const arrayToList =(array)=>{
+    let list = {};
+    let rest = null;
+    list ={ value: array[0],
+    rest:{
+        value: array[1],
+        rest: {
+            value: array[2],
+            rest: rest
+        }
+    }}
+}
+// start at end? spread object into object?
+const arrayToList2 = (array)=>{
+    let list = {}
+array.forEach(element => {
+    
+});
+return console.log(list)
+}
+arrayToList2([1, 2, 3]);
--- journal-eloquent.js
+++ journal-eloquent.js
@@ -234,7 +234,59 @@ for (let i = 0; i < JOURNAL.length; i++) {
   const entry = JOURNAL[i];
   console.log(`${entry.events.length} events for loop`);
 }
-
+// same effect, more concise:
 for (const entry of JOURNAL) {
   console.log(`${entry.events.length} events`);
+}
+
+// create two-by-two table for a journal event
+function tableFor(event, journal) {
+  let table = [0, 0, 0, 0];
+  for (let i = 0; i < journal.length; i++) {
+    let entry = journal[i],
+      index = 0;
+    if (entry.events.includes(event)) index += 1;
+    if (entry.squirrel) index += 2;
+    table[index] += 1;
+  }
+  return table;
+}
+
+// compute phi coefficient from array
+function phi(table) {
+  return (
+    (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt(
+      (table[2] + table[3]) *
+        (table[0] + table[1]) *
+        (table[1] + table[3]) *
+        (table[0] + table[2])
+    )
+  );
+}
+
+// create an array of all events in journal
+function journalEvents(journal) {
+  let events = [];
+  for (let entry of journal) {
+    for (let event of entry.events) {
+      if (!events.includes(event)) {
+        events.push(event);
+      }
+    }
+  }
+  return events;
+}
+
+// using array of all events calculate phi coefficient for each
+for (let event of journalEvents(JOURNAL)) {
+  console.log(event + ":", phi(tableFor(event, JOURNAL)));
+}
+
+// get just correlations greater than 0.1 or less than -0.1 to look for statistically significant events
+for (let event of journalEvents(JOURNAL)) {
+  let correlation = phi(tableFor(event, JOURNAL));
+  if (correlation > 0.1 || correlation < -0.1) {
+    console.log(event + ":", correlation);
+  }
 }
