97b170d2a8116f73c61552937804b2708433b0ea
travis <trvsmartin@gmail.com>
2023-01-03 11:21:48 -0800

    start chapter 4 notes & journal playground
    <body>
    
    Issue #

---
+++ data-structures-eloquent.js
@@ -0,0 +1,15 @@
+/*
+Chapter 4: Data Structures: Objects and Arrays
+
+Square bracket notation: square brackets immediately following an expression with an expression inside looks up the element in left hand expression matching the index given by the expression in brackets
+Dot notation: the word following dot is name of property to look up.  vs square brackets where the expression is evaluated, then converted to string
+-Dot notation works only with properties that look like variable names, thus to access property "some property" use bracket notation: array["some property"]
+Methods: a property that holds a function as its value
+Objects: collections of properties.  curly braces at the start of statement commence block of statement, otherwise the describe an object
+
+<in>: a binary operater. applied to string and object tells if that object has a property by that name
+Object.assign: copies all properties from one object into another
+
+<for-of> loop: works for arrays, strings, some others
+
+*/
