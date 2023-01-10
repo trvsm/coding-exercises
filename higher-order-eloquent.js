2392f6ad3717d6af286d002980e46bacb28c9145
Travis Martin <trvsmartin@gmail.com>
2023-01-10 06:52:10 -0800

    start ch 5 notes

---
+++ higher-order-eloquent.js
@@ -0,0 +1,11 @@
+/*
+Chapter 5: Higher Order Functions
+repeat something n times:
+function repeat(n, action){
+    for(let i = 0; i < n; i++){
+        action(i);
+    }
+}
+Higher order functions allow us to abstract over actions, not just values
+The goal is more logical code that breaks to small chunks, easy to understand
+*/
